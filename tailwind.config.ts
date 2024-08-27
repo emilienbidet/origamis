68a35c982e839fafa164dd03fc792d5332ff9533
Emilien Bidet <emilienbdt@gmail.com>
2024-08-27 13:34:25 +0200

    Add default background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
